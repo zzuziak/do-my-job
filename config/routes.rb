47428cb5964890913c2f3173d74f96ea0870ad88
Julian Schleiminger <j.schleiminger@gmail.com>
2018-08-14 14:23:21 +0200

    add owned_jobs and route and view

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   resources :jobs, only: [:index, :create,:edit, :update, :show, :destroy] do
     resources :bookings, only: [:new, :create]
   end
+  get 'owned_jobs', to: "dashboard_owned_jobs#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
