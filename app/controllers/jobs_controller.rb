6f0956fa68055a81fbcc1cf89c1b1fe823b3f8c7
Zuza Zuber <zuberzuzanna@gmail.com>
2018-08-14 11:36:53 +0200

    update new and create actions

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,22 @@
 class JobsController < ApplicationController
+  def new
+    authorize @job
+    @job = Job.new
+  end
+
+  def create
+    authorize @job
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to job_path(@job)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :category, :date, :price, :description, :guided, :photo, :location)
+  end
 end
--- app/policies/job_policy.rb
+++ app/policies/job_policy.rb
@@ -1,4 +1,16 @@
 class JobPolicy < ApplicationPolicy
+
+
+  def new?
+    create?
+  end
+
+  def create?
+    true
+  end
+
+  private
+
   class Scope < Scope
     def resolve
       scope.all
