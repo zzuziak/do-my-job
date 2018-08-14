db3027367980dbfc92bfe983fe951f3af75fb8ac
Julian Schleiminger <j.schleiminger@gmail.com>
2018-08-14 11:03:21 +0200

    create the correct routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :users, only: [:show] do
+    resources :jobs, only:  [:new, :create, :edit, :update, :destroy]
+    resources :bookings, only: [:index, :show, :edit, :update, :destroy]
+  end
+  resources :jobs, only: [:index, :show] do
+    resources :bookings, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
